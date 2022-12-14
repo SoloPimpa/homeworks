4595a89776aebaf1465ddc707db1bc7dddd26bf5
SoloPimpa <sdarom.sdarom@gmail.com>
2022-12-14 17:55:42 +0200

    hw23

--- my-app/src/services/todoService.js
+++ my-app/src/services/todoService.js
@@ -1,30 +1,31 @@
 const TODO_URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/todos/';
-export function getTodo(){
+
+export function getTodo() {
     return fetch(TODO_URL).then(res => res.json());
 }
 
-export function deleteTodo(id){
-    return fetch(TODO_URL+id,{
+export function deleteTodo(id) {
+    return fetch(TODO_URL + id, {
         method: 'DELETE',
     }).then(res => res.json());
 }
-export function createTodo(newTodo){
-    return fetch(TODO_URL,{
-        method:'POST',
+
+export function createTodo(newTodo) {
+    return fetch(TODO_URL, {
+        method: 'POST',
         body: JSON.stringify(newTodo),
-        headers:{
-            'Content-Type':'application/json',
+        headers: {
+            'Content-Type': 'application/json',
         }
     }).then(res => res.json());
 }
 
-export function toggleTodo(id){
-    const isDone ={isDone:true}
-
+export function toggleTodo(id) {
+    let isDone = {isDone:true};
     return fetch(TODO_URL + id, {
         method: 'PUT',
         body: JSON.stringify(isDone),
-        headers:  {
+        headers: {
             'Content-Type': 'application/json',
         },
     }).then(res => res.json());
