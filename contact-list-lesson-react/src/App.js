b9e007fd4a64b7f17d31c16d2ecd54ae011dbf2e
SoloPimpa <sdarom.sdarom@gmail.com>
2022-12-20 19:55:40 +0200

    class-work

---
+++ contact-list-lesson-react/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {useState} from "react";
+import Users from "./modules/users/components/Users";
+import Navigation from "./modules/common/components/Navigation";
+import Posts from "./modules/posts/components/Posts";
+
+function App() {
+  const [page, setPage] = useState('posts')
+  return (
+      <div className="container">
+          <Navigation page={page} navigate={setPage}/>
+          {page === 'users' ? <Users/> : null}
+          {page === 'posts' ? <Posts/> : null}
+      </div>
+  );
+}
+
+export default App;
---
+++ contact-list-lesson-react/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import './modules/common/css/normalize.css';
+import './modules/common/css/dark-theme.css';
+import './modules/common/css/skeleton.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
