0cacb23ab2e28833929177f0262b6d39cd01bc5a
SoloPimpa <sdarom.sdarom@gmail.com>
2022-12-20 20:06:18 +0200

    class-work

--- contact-list-lesson-react/src/modules/users/components/UsersForm.js
+++ contact-list-lesson-react/src/modules/users/components/UsersForm.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 function UsersForm({onSave, currentUser}) {
-    function onFormSubmit (e){
+
+    const formRef = useRef();
+
+    function onClick (e){
         e.preventDefault();
 
-        const form = e.target.elements;
+        const form = formRef.current.elements;
 
         onSave({
             id: form.id.value,
@@ -13,17 +16,33 @@ function UsersForm({onSave, currentUser}) {
             email: form.email.value,
         });
 
-        e.target.reset();
+        form.current.reset();
     }
 
+    // function onFormSubmit (e){
+    //     e.preventDefault();
+    //
+    //     const form = e.target.elements;
+    //
+    //     onSave({
+    //         id: form.id.value,
+    //         name: form.name.value,
+    //         surname: form.surname.value,
+    //         email: form.email.value,
+    //     });
+    //
+    //     e.target.reset();
+    // }
+
     return (
-        <form className="row" onSubmit={onFormSubmit}>
+        <form className="row" ref={formRef}>
+
             <div className="three columns">
-                <input type="text" name="id" defaultValue={currentUser.id}/>
+                <input type="hidden" name="id" defaultValue={currentUser.id}/>
                 <input type="text" defaultValue={currentUser.name} name="name" placeholder="Name"/></div>
             <div className="three columns"><input type="text" defaultValue={currentUser.surname} name="surname" placeholder="Surname"/></div>
             <div className="three columns"><input type="text" defaultValue={currentUser.email} name="email" placeholder="Email"/></div>
-            <div className="three columns"><button>Save</button></div>
+            <div className="three columns"><button onClick={onClick} type="button">Save</button></div>
         </form>
     );
 
