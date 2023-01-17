fdb3afa97ee8f4920994f1f5c5da3442c211522f
SoloPimpa <sdarom.sdarom@gmail.com>
2023-01-17 23:11:45 +0200

    hw-30

--- my-shop/src/modules/common/auth/pages/Login.js
+++ my-shop/src/modules/common/auth/pages/Login.js
@@ -3,8 +3,9 @@ import {Formik, Form} from "formik";
 import {Navigate, NavLink} from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import loginValidationSchema from "../validation/loginValidationSchema";
-import {Box, Button, Checkbox, Container, FormControlLabel} from "@mui/material";
+import {Box, Button, Checkbox, Container, FormControlLabel, Typography} from "@mui/material";
 import MyTextField from "../components/form/MyTextField";
+import {LockOutlined} from "@mui/icons-material";
 
 const initialValues = {username: '', password: '', role: ''};
 
@@ -23,6 +24,9 @@ function Login() {
                 flexDirection: 'column',
                 alignItems: 'center',
             }}>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
                 <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={loginValidationSchema}>
                     {(props) => console.log(props) || (
                         <Form>
@@ -40,11 +44,10 @@ function Login() {
                                           label="Password"
                                           type="password"
                                           autoComplete="current-password"/>
-                            <MyTextField name="role" placeholder="Role" fullWidth margin="normal"></MyTextField>
+                            <MyTextField name="role" placeholder="Role" fullWidth margin="normal"/>
                             <FormControlLabel
                                 control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
+                                label="Remember me"/>
                             <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>Login</Button>
                             <Button to="/" component={NavLink} fullWidth>Cancel</Button>
                         </Form>
